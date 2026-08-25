8638f17cea9e622f2f87b3fbf54ed7843bf1e875
Kaue <kkas1@aluno.ifal.edu.br>
2026-08-25 16:46:04 -0300

    Refactor DBHelper class and database initialization

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,21 +1,19 @@
-import 'dart:async'; // Importa a biblioteca para operações assíncronas (que demoram um tempo)
-import 'package:path/path.dart'; // Importa a biblioteca para lidar com caminhos de pastas no celular
-import 'package:sqflite/sqflite.dart'; // Importa o pacote oficial do banco de dados SQLite no Flutter
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-class DBHelper { // cria a classe que vai gerenciar o banco de dados
-  Future<Database> initDB() async { // função assincrona que retorna o banco
+class DBHelper {
+  Future<Database> initDB() async {
     String path = await getDatabasesPath();
-    String dbName = 'financeiro.db'; // define nome do arquivo
+    String dbName = 'financeiro.db';
 
-    String dbPath = join(path, dbName); // junta a pasta com o nome do arquivo
+    String dbPath = join(path, dbName);
 
-    // abre o banco. Se nao existir, cria com o onCreateDB
     Database db = await openDatabase(dbPath, version: 1, onCreate: onCreateDB);
 
-    return db; // retorna o banco
+    return db;
   }
 
-  // função que roda na primeira vez que o app é instalado para criar a tabela
   FutureOr<void> onCreateDB(Database db, int version) async {
     String sql = ''' CREATE TABLE transacoes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,8 +22,8 @@ class DBHelper { // cria a classe que vai gerenciar o banco de dados
             isEntrada INTEGER,
             categoria TEXT,
             data TEXT
-        );'''; // O comando em sql para criar a tabela com suas colunas e tipos de dados
+        );''';
 
-    await db.execute(sql); // executa o comando sql acima direto no banco
+    await db.execute(sql);
   }
 }
