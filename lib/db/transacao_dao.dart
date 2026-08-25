82ad08e6da11e0287c867c6f63c729481224399c
Kaue <kkas1@aluno.ifal.edu.br>
2026-08-25 16:45:51 -0300

    Refactor TransacaoDao for cleaner code

--- lib/db/transacao_dao.dart
+++ lib/db/transacao_dao.dart
@@ -1,26 +1,21 @@
-import 'package:sqflite/sqflite.dart'; // Importa as ferramentas nativas do SQLite
-import '../db/db_helper.dart'; // Importa a nossa classe que sabe abrir o banco
-import '../domain/transacao.dart'; // Importa a classe modelo de Transação
+import 'package:sqflite/sqflite.dart';
+import '../db/db_helper.dart';
+import '../domain/transacao.dart';
 
-class TransacaoDao { // cria a classe que concentra as regras de acesso a dados
+class TransacaoDao {
 
-  // função assíncrona que recebe uma transação e a salva no banco
   Future<int> inserir(Transacao transacao) async {
-    Database db = await DBHelper().initDB(); // pede para o DBhelper abrir o banco e aguarda
-    return await db.insert('transacoes', transacao.toJson()); // Usa a função nativa 'insert' passando o objeto convertido (toJson)
+    Database db = await DBHelper().initDB();
+    return await db.insert('transacoes', transacao.toJson());
   }
-
-  // função assíncrona que busca tudo no banco e devolve uma lista de transações
   Future<List<Transacao>> listarTodas() async {
-    Database db = await DBHelper().initDB(); // Abre o banco e aguarda
-    // Faz a consulta (SELECT) na tabela, ordenando pelo ID do maior pro menor (DESC = Decrescente)
+    Database db = await DBHelper().initDB();
     final List<Map<String, dynamic>> mapas = await db.query('transacoes', orderBy: 'id DESC');
 
-    List<Transacao> listaTransacoes = []; // Cria uma lista vazia para guardar os resultados
-    for (var mapa in mapas) { // Laço de repetição que passa por cada item retornado pelo banco
-      listaTransacoes.add(Transacao.fromJson(mapa)); // Converte cada item em Transacao e adiciona na lista
+    List<Transacao> listaTransacoes = [];
+    for (var mapa in mapas) {
+      listaTransacoes.add(Transacao.fromJson(mapa));
     }
-
-    return listaTransacoes;// Retorna a lista final cheia para a tela que a solicitou
+    return listaTransacoes;
   }
 }
