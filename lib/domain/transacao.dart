3e9c996a2c29c9d541ce87d038bee0c7b659d958
Kaue <kkas1@aluno.ifal.edu.br>
2026-08-25 13:59:42 -0300

    Refactor Transacao class for improved JSON handling

--- lib/domain/transacao.dart
+++ lib/domain/transacao.dart
@@ -1,13 +1,13 @@
-class Transacao { // define o "molde" para as transações
-  int? id; // ? opcional
-  late String titulo; // variavel late ( inicializada depois)
-  late double valor; // variavel late ( inicializada depois)
-  late bool isEntrada; // variavel late ( inicializada depois)
-  late String categoria; // variavel late ( inicializada depois)
-  late DateTime data; // variavel late ( inicializada depois)
+class Transacao {
+  int? id;
+  late String titulo;
+  late double valor;
+  late bool isEntrada;
+  late String categoria;
+  late DateTime data;
 
-  Transacao({ // construtor padrao: exige que passe os dados ao criar uma nova transação
-    this.id, // nao tem required porque o banco gera sozinho
+  Transacao({
+    this.id,
     required this.titulo,
     required this.valor,
     required this.isEntrada,
@@ -15,25 +15,25 @@ class Transacao { // define o "molde" para as transações
     required this.data,
   });
 
-  // pega um mapa(JSON vindo do sqlite) e transforma no objeto transação
+  // Converte o Map vindo do SQLite para o objeto Transacao
   Transacao.fromJson(Map<String, dynamic> json) {
-    id = json['id']; // Pega o número do mapa e salva no ID
-    titulo = json['titulo']; // Pega o texto do mapa e salva no titulo
-    valor = json['valor']; // Pega o número decimal do mapa e salva no valor
-    isEntrada = json['isEntrada'] == 1; // Se no banco for o número 1, vira true. Se não, false.
-    categoria = json['categoria']; // Pega o texto do mapa e salva na categoria
-    data = DateTime.parse(json['data']); // Converte o texto do banco de volta para o formato de Data do Dart
+    id = json['id'];
+    titulo = json['titulo'];
+    valor = json['valor'];
+    isEntrada = json['isEntrada'] == 1;
+    categoria = json['categoria'];
+    data = DateTime.parse(json['data']);
+  }
 
-  // Função que pega o objeto Transacao e converte num Mapa para salvar no SQLite
+  // Converte o objeto Transacao para Map para salvar no SQLite
   Map<String, dynamic> toJson() {
-    Map<String, dynamic> json = {}; // cria um mapa vazio
-    json['id'] = id; // guarda o ID no mapa
-    json['titulo'] = titulo;
-    json['valor'] = valor;
-    json['isEntrada'] = isEntrada ? 1 : 0;
-    json['categoria'] = categoria;
-    json['data'] = data.toIso8601String();
-
-    return json; // devolve o mapa pronto para ser gravado no banco de dados
+    Map<String, dynamic> jsonMap = {};
+    if (id != null) jsonMap['id'] = id;
+    jsonMap['titulo'] = titulo;
+    jsonMap['valor'] = valor;
+    jsonMap['isEntrada'] = isEntrada ? 1 : 0;
+    jsonMap['categoria'] = categoria;
+    jsonMap['data'] = data.toIso8601String();
+    return jsonMap;
   }
 }
