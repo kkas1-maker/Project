9bf7de71dc8603099eb8593a64117aa72ad5b9e4
Kaue <kkas1@aluno.ifal.edu.br>
2026-08-25 16:44:07 -0300

    Refactor main.dart for improved readability

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart'; // Pacote base de visual do Flutter
-import 'screens/tela_principal.dart'; // Importa a tela inicial
+import 'package:flutter/material.dart';
+import 'screens/tela_principal.dart';
 
-void main() { // função principal
-  runApp(const MyApp()); // roda o widget raiz o app
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // Construtor padrão
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) { // Constrói o visual base
-    return MaterialApp( // Aplica o design Material (padrão Google)
-      title: 'Cofrinho', // Título interno do app
-      debugShowCheckedModeBanner: false, // Remove a faixa chata de debug da tela
-      theme: ThemeData( // Define as cores e fontes do aplicativo
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal), // Cor base: Teal (verde-azulado)
-        useMaterial3: true, // Usa os componentes visuais mais modernos do Flutter
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cofrinho',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
       ),
-      home: const TelaPrincipal(), // Define que a primeira tela ao abrir o app é a TelaPrincipal
+      home: const TelaPrincipal(),
     );
   }
 }
